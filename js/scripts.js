942eb0124e051a7f8e0aeed472c15e6931f6c0e4
Matthew Terrill <meterrill@gmail.com>
2015-02-13 20:34:49 +0000

    add html and jquery

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,34 @@ var triangleChecker = function(side1, side2, side3) {
     return false;
   }
 };
+
+$(document).ready(function() {
+  $("form#triangle-checker").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+
+    var numberOfSides = triangleChecker(side1, side2, side3);
+    if (numberOfSides === 1) {
+      var typeOfTriangle = "a scalene";
+      $(".typeOfTriangle").text(typeOfTriangle);
+      $("#valid").show();
+      $("#not-valid").hide();
+    } else if (numberOfSides === 2) {
+      var typeOfTriangle = "an isosceles";
+      $(".typeOfTriangle").text(typeOfTriangle);
+      $("#valid").show();
+      $("#not-valid").hide();
+    } else if (numberOfSides === 3) {
+      var typeOfTriangle = "an equilateral";
+      $(".typeOfTriangle").text(typeOfTriangle);
+      $("#valid").show();
+      $("#not-valid").hide();
+    } else {
+      $("#not-valid").show();
+      $("#valid").hide();
+    }
+
+    event.preventDefault();
+  });
+});
